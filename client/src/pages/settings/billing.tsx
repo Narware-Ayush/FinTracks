5535b4925dfe9668f076c3e64ab594dec5fe66ec
Narware-Ayush <ayushnarware3@gmail.com>
2025-11-22 11:22:19 +0530

    Initial commit - my version

---
+++ client/src/pages/settings/billing.tsx
@@ -0,0 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+
+const Billing = () => {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Billing</h3>
+        <p className="text-sm text-muted-foreground">
+          Manage your  billing information
+        </p>
+      </div>
+      <Separator />
+
+      
+      <div> <h2>I Am currently working on this ... !</h2></div>
+    </div>
+  );
+};
+
+export default Billing;
